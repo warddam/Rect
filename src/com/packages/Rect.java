dd7f3b0af8d76e6bc25f60ab8f3d999480b0952d
DJ Ward <warddam001@warren.k12.in.us>
2022-01-20 09:47:22 -0500

    update rect

---
+++ src/com/packages/Rect.java
@@ -0,0 +1,15 @@
+package com.packages;
+
+public class Rect {
+
+    public static void main(String[] args) {
+
+        RectAttrib rect1 = new RectAttrib();
+
+        rect1.setHeight(4);
+        rect1.setWidth(7);
+ System.out.println(rect1.getHeight());
+        System.out.println(rect1.getWidth());
+    }
+
+}
---
+++ src/com/packages/RectAttrib.java
@@ -0,0 +1,27 @@
+package com.packages;
+
+public class RectAttrib {
+   private double height;
+    double width;
+
+    public void setHeight(double ht)
+    {
+
+        height = ht;
+
+
+    }
+    public double getHeight(){
+
+        return height;
+    }
+    public void setWidth (double wd)
+    {
+        width = wd;
+
+    }
+    public double getWidth() {
+
+        return width;
+    }
+}
